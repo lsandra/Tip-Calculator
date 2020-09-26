8429bf1dc7ba195d9f6a3909a6e6cbbede635a43
lsandra <57500208+lsandra@users.noreply.github.com>
2020-09-26 11:15:59 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+//get bill input 
+const tipForm = document.getElementById("tip-form");//global scope
+
+tipForm.addEventListener("submit", function(e){
+    e.preventDefault();
+    const billAmount = NUmber(document.querySelector("#bill-amount").value);//scoped to function,
+    //function scope
+    const tipPercentage = Number(document.querySelector("#tip-amount").value);
+    //do some math for the tip
+    //do some math for the tip plus bill
+    const tipAmount = document.querySelector("#final-tip");
+    tipAmount.innerHTML = '${tipPercent}-'
+})
+
